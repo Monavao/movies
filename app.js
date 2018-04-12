ccd2355ca5b92601ce70d12413d73047bd594999
Monavao <omid.monavari@hotmail.fr>
2018-04-12 12:41:22 +0200

    Mise en place routes

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/public',express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-	res.send('Hello world !!!');
+	res.render('index');
+});
+
+app.get('/movies', (req, res) => {
+	res.render('movies');
+});
+
+app.get('/movies/add', (req, res) => {
+	res.send('Formulaire en construction');
+});
+
+app.get('/movies/:id', (req, res) => {
+	const id = req.params.id;
+	const title = 'Terminator';
+
+	res.render('movies-details', { 
+		moviesId: id,
+		movieTitle: title
+	});
 });
 
 app.listen(port, () => {
